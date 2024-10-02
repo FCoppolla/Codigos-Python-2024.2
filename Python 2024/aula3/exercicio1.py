4f4b0eb81e3cb2406c52a318274d61c81edffbbe
FCoppolla <felipe.v.coppolla@gmail.com>
2024-10-02 20:55:37 -0300

    exercicio1
    calculou o IPVA de um carro

---
+++ Python 2024/aula3/exercicio1.py
@@ -0,0 +1,5 @@
+#faça um programa que calcule o ipva de um carro com base no valor do mesmo. IPVA é 4% do valor do carro 
+print("vamos calcular o valor do IPVA de um carro!")
+valordocarro = float(input("coloque o valor do carro"))
+IPVA = valordocarro *0.04
+print(f"o valor que será pago de IPVA é de: {IPVA} ")
