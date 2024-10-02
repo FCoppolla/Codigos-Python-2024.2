fc09305abbfaf22b84873406eab761b68398c16a
FCoppolla <felipe.v.coppolla@gmail.com>
2024-10-02 19:29:42 -0300

    organizei melhor os arquivos
    para melhor entendimento do conteudo

---
+++ Python 2024/aula2/Evento1.py
@@ -0,0 +1,9 @@
+#esses serão os passos do pirata 
+print("este é o caminho para o mapa do tesouro")
+print("andar para a direita")
+print("andar para esquerda")
+print("andar para baixo")
+print("procurar o x e cavar")
+
+
+
