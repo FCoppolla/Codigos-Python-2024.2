478b280d42d673f1c3eb8554c0780a667a716508
FCoppolla <felipe.v.coppolla@gmail.com>
2024-10-02 23:11:36 -0300

    exercicio 4 feito
    o exercicio de casa foi feito com sucesso

---
+++ Python 2024/aula3/exercicio4.py
@@ -0,0 +1,15 @@
+#peça o usuario o tamanho do aro da bicicleta e ver o que ele deseja comprar com base nisso:
+#recomendar o tamanho do guidão, da manete e quadro de bike
+#o guidão tem /2 do aro. manete 1/4 do aro da roda. o quadro será o mesmo do aro 
+
+print("vamos juntar informação para a sua bike?")
+aro = float(input("qual o tamanho do aro da sua Bike??  "))
+guidão = aro/2
+manete = aro/4
+quadrobike = aro
+
+print(f"o tamanho do guidão é : {guidão}")
+print(f"o tamanho da manete é : {manete}")
+print(f"o tamanho do guadro da sua bike é: {quadrobike}")
+
+print("obrigado por fazer a pesquisa! ")
