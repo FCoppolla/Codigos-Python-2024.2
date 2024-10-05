25cbc95de9257d50b8be3a030858131163294d3e
FCoppolla <felipe.v.coppolla@gmail.com>
2024-10-04 21:37:04 -0300

    atualização

--- Python 2024/aula3/exercicio4.py
+++ Python 2024/aula3/exercicio4.py
@@ -3,7 +3,7 @@
 #o guidão tem /2 do aro. manete 1/4 do aro da roda. o quadro será o mesmo do aro 
 
 print("vamos juntar informação para a sua bike?")
-aro = int(input("qual o tamanho do aro da sua Bike?? "))
+aro = float(input("qual o tamanho do aro da sua Bike?? "))
 guidão = aro/2
 manete = aro/4
 quadrobike = aro
--- Python 2024/aula4/exercicio1.py
+++ Python 2024/aula4/exercicio1.py
@@ -3,7 +3,7 @@ print("vamos nos escrever para a aula de Python?")
 idade = int(input("qual a sua idade?"))
 
 if(idade>=16):
-    print("sim, você se matricular para aula!")
+    print("sim, você pode se matricular para aula!")
 
 else:
         print("Não, você não pode se matricular")
