d63d0f2cdfc6ddaad3fa72fc8f0c940490571f28
FCoppolla <felipe.v.coppolla@gmail.com>
2024-10-07 14:31:09 -0300

    fazendo o dever de casa e corrigindo erros
    Eu fiz o dever de casa corrigindo tudo que estava errado

--- Python 2024/aula4/deverdecasa.py
+++ Python 2024/aula4/deverdecasa.py
@@ -1,3 +1,16 @@
-print("faça um programa qie doga se um numero é divisivel por 3 e que diga se um numero é divisivel por 5")
+#faça um programa que diga se um numero é divisivel por 3 e que diga se um numero é divisivel por 5")
 
-print("vai no gás ! ")
+num = int(input("entre com um número para saber se é divisivel por 3 ou 5?"))
+
+if ( num % 3 == 0):
+    print(f"o {num} é divisivel por 3!")
+else:
+     print(f"{num} não é divisível por 3.")
+if ( num % 5 == 0 ):
+    print(f"o {num} é divisivel por 5 ")
+
+else: 
+    print(f"{num} não é divisível por 5.")
+    
+
+    
